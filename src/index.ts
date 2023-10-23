997844be08265b44ac8ce031f19634b7b01f5c76
Herr Turing <admin@nda.software>
2023-10-23 09:27:49 -0300

    Fixed mistake in parameters' typing.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { indexer, type indexerOptions } from 'ts-cornucopia/scripts';
 
 import { type Plugin } from 'rollup';
 
-export default function (path: string | [string], options: indexerOptions = {}): Plugin {
+export default function (path: string | string[], options: indexerOptions = {}): Plugin {
     return {
         name: 'indexer',
         version: '1.0.0',
